e68d112e03b9383788bf4defdcb24810bda39923
Roland Ibiwoye <contact@rolandayo.com>
2025-02-03 23:55:35 +0100

    routes added

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 });
+
 
 
