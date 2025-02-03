3124aea560e630302804c4495fa34e1176465473
Roland Ibiwoye <contact@rolandayo.com>
2025-02-03 23:54:10 +0100

    express setup

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import router from './routes/index.js';
+
+const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+
